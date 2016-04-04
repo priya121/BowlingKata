3721d0f046eb064571dcf947e26e142a8b546138
priya121 <priya121@github.com>
2016-04-04 11:10:26 +0100

    Extracts method for calculating scores.

--- src/main/BowlingGame.java
+++ src/main/BowlingGame.java
@@ -12,21 +12,38 @@ public class BowlingGame {
         int score = 0;
         int frameIndex = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (knockedPins[frameIndex] == 10) {
-                score += 10 + knockedPins[frameIndex + 1] + knockedPins[frameIndex + 2];
+            if (isStrike(frameIndex)) {
+                score += strikeBonus(frameIndex);
                 frameIndex++;
             } else if (isSpare(frameIndex)) {
-                score += 10 + knockedPins[frameIndex + 2];
+                score += spareBonus(frameIndex);
                 frameIndex += 2;
             } else {
-                score += knockedPins[frameIndex] + knockedPins[frameIndex + 1];
+                score += normalScore(frameIndex);
                 frameIndex += 2;
             }
         }
         return score;
     }
 
+    private int normalScore(int frameIndex) {
+        return knockedPins[frameIndex] + knockedPins[frameIndex + 1];
+    }
+
+    private int spareBonus(int frameIndex) {
+        return 10 + knockedPins[frameIndex + 2];
+    }
+
+    private int strikeBonus(int frameIndex) {
+        return 10 + knockedPins[frameIndex + 1] + knockedPins[frameIndex + 2];
+    }
+
     private boolean isSpare(int i) {
-        return knockedPins[i] + knockedPins[i + 1] == 10;
+        return normalScore(i) == 10;
+    }
+
+    private boolean isStrike(int i) {
+        return knockedPins[i] == 10;
+
     }
 }
