3f117c4947ad20c65e3a50bc001a915800e1437d
priya121 <priya121@github.com>
2016-04-04 09:06:45 +0100

    Adds helper method to tests.

--- src/test/BowlingGameTest.java
+++ src/test/BowlingGameTest.java
@@ -1,27 +1,36 @@
 package test;
 
 import main.BowlingGame;
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
 
 public class BowlingGameTest {
+    BowlingGame game = new BowlingGame();
+
+    @Before
+    public void setUp() {
+        game = new BowlingGame();
+    }
 
     @Test
     public void gutterGameScoreOfZero() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        multipleRolls(20, 0);
         assertEquals(0, game.score());
     }
-    
+
     @Test
     public void onePinKnockedDownEveryRoll() {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        multipleRolls(20, 1);
         assertEquals(20, game.score());
     }
+
+    public void multipleRolls(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++) {
+            game.roll(pins);
+        }
+
+    }
+
 }
