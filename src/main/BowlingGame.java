1a07c8b9d9cc62ace5e33eeb6668fd4a16f3ecf3
priya121 <priya121@github.com>
2016-04-04 08:56:54 +0100

    Adds test for one pin knocked down every roll.

--- src/main/BowlingGame.java
+++ src/main/BowlingGame.java
@@ -1,10 +1,13 @@
 package main;
 
 public class BowlingGame {
+    private int score;
+
     public void roll(int pins) {
+        score += pins;
     }
 
     public int score() {
-        return 0;
+        return score;
     }
 }
--- src/test/BowlingGameTest.java
+++ src/test/BowlingGameTest.java
@@ -6,12 +6,22 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class BowlingGameTest {
+
     @Test
-    public void testsGutterGame() {
+    public void gutterGameScoreOfZero() {
         BowlingGame game = new BowlingGame();
         for (int i = 0; i < 20; i++) {
             game.roll(0);
         }
         assertEquals(0, game.score());
+    }
+    
+    @Test
+    public void onePinKnockedDownEveryRoll() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        assertEquals(20, game.score());
     }
 }
